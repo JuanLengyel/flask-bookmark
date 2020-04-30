9e83c177cf71a379de83e64e8add123ededde205
Juan-Lengyel <juan.lengyel@globant.com>
2020-04-30 14:41:12 -0500

    Added custom error pages

--- bookmark.py
+++ bookmark.py
@@ -17,3 +17,11 @@ def index():
 @app.route('/add')
 def add_bookmark():
   return render_template('add.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(error):
+  return render_template('500.html'), 500
